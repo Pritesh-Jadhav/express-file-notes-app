bb1706329c39f7a1ae9b23339c29a7f243fc1250
Pritesh-Jadhav <priteshjadhav400@gmail.com>
2026-06-24 17:24:03 +0530

    Added Express file based notes app

---
+++ script.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express()
+const path = require("path");
+const fs = require("fs");
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+
+    fs.readdir(`./files`,(err, files)=>{
+      res.render("index",{files:files})
+    })
+
+})
+
+app.post("/create",(req,res)=>{
+
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.title,(err)=>{
+        res.redirect("/")
+    })
+
+})
+
+app.listen(6969,function(){
+    console.log("working")
+});
