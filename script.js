a94e4b95ab36869fa513c7e4c73dd13a510e15ef
Pritesh-Jadhav <priteshjadhav400@gmail.com>
2026-07-01 11:47:10 +0530

    Added edit functionality to notes app

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express()
 const path = require("path");
 const fs = require("fs");
+const { log } = require("console");
+const { fileLoader } = require("ejs");
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.json());
@@ -17,13 +19,59 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/create",(req,res)=>{
-
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.title,(err)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}`,req.body.textarea,(err)=>{
         res.redirect("/")
     })
 
 })
 
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render("show",{filename:req.params.filename , filedata:filedata})
+    })
+
+})
+
+app.get("/edit/:filename",(req,res)=>{
+    
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+
+        res.render("edit",{filename:req.params.filename,filedata:filedata})
+    })
+
+})
+
+app.post("/edit/:filename", (req, res) => {
+
+    const oldFilename = req.params.filename;
+
+    const newFilename = req.body.title.split(" ").join("") ;
+
+    fs.rename(
+        `./files/${oldFilename}`,
+        `./files/${newFilename}`,
+        (err) => {
+
+            if (err) return res.send(err);
+
+            fs.writeFile(
+                `./files/${newFilename}`,
+                req.body.textarea,
+                (err) => {
+
+                    if (err) return res.send(err);
+
+                    res.redirect("/");
+
+                }
+            );
+
+        }
+    );
+
+});
+
 app.listen(6969,function(){
     console.log("working")
 });
+
